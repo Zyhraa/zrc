87459291d91e97a69031a0f5684920ccc49134f6
Zhyraa <roralolxd@gmail.com>
2023-02-05 18:18:22 +0700

    nav

--- assets/header.php
+++ assets/header.php
@@ -6,6 +6,38 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Document</title>
+   <!-- font -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=News+Cycle:wght@400;700&family=Playfair+Display:wght@600;700;800&display=swap" rel="stylesheet">
+   <!-- font -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+   <script src="https://kit.fontawesome.com/5681f6e595.js" crossorigin="anonymous"></script>
+   <link rel="stylesheet" type="text/css" href="assets/css/index.css">
 </head>
 
-<body>
+<body style="background-color: black;">
+   <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+         <a class="navbar-brand" href="home.php"><img src="assets/images/graylogo.png" style="size: 1px;" alt="logo"></a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+               <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </nav>
--- index.php
+++ index.php
@@ -6,9 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>zadecrc</title>
+    <!-- font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=News+Cycle:wght@400;700&family=Playfair+Display:wght@600;700;800&display=swap" rel="stylesheet">
+    <!-- font -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/5681f6e595.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="assets/css/index.css">
