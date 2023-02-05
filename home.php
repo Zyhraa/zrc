e8fca10e9a8932924dd761df7509faa79190ea72
Zhyraa <roralolxd@gmail.com>
2023-02-05 21:22:24 +0700

    bingung bg home hehe

--- home.php
+++ home.php
@@ -1,3 +1,4 @@
 <?php include 'assets/header.php' ?>
-
+<div class="bg-image" style="background-image: url('assets/images/home.png'); background-size: cover;  background-repeat:no-repeat; height: 100vh">
+</div>
 <?php include 'assets/footer.php' ?>
