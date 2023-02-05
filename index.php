59f721802117bbcc05800fd6f638ce293b96cd3d
Zhyraa <roralolxd@gmail.com>
2023-02-05 16:44:13 +0700

    link to home diganti php

--- index.php
+++ index.php
@@ -8,17 +8,13 @@
     <title>zadecrc</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=News+Cycle:wght@400;700&family=Playfair+Display:wght@600;700;800&display=swap"
-        rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=News+Cycle:wght@400;700&family=Playfair+Display:wght@600;700;800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/5681f6e595.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="assets/css/index.css">
 </head>
 
-<body class="bg-image"
-    style="background-image: url('assets/images/splashh.png') ; background-size: cover; background-repeat: no-repeat;">
+<body class="bg-image" style="background-image: url('assets/images/splashh.png') ; background-size: cover; background-repeat: no-repeat;">
     <div class=" container-fluid">
         <div class="row">
             <div class="col-12 my-3">
@@ -31,7 +27,7 @@
                     <h1>ZADDE ROYAL CROWN</h1>
                     <p>Your Journey Starts Here</p>
                 </div>
-                <a class="btn mt-5 px-5 py-3" href="home.html">About Us</a>
+                <a class="btn mt-5 px-5 py-3" href="home.php">About Us</a>
             </div>
         </div>
     </div>
@@ -58,9 +54,7 @@
     </footer>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 
