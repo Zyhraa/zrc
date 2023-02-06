8bfca1c682f5c62f2f7d65429fc62cd514e13425
Aurora <87532858+Zyhraa@users.noreply.github.com>
2023-02-06 22:51:06 +0700

    Update footer.php

--- assets/footer.php
+++ assets/footer.php
@@ -1,3 +1,29 @@
+<div style="background-color: #5C5024; color:white">
+    <div class="container d-flex align-items-center pt-2">
+        <div class="col col-6 alamat">
+            <div class="d-flex align-items-center">
+                <i class="fas fa-map-marker-alt me-3"></i>
+                <p class="m-0 p-0">MTH Square Ground Floor (GF) A4 A, Jl. Letjen
+                    M. T. Haryono Kav. 10,Jakarta Timur, 13330</p>
+            </div>
+        </div>
+        <div class="col col-4 alamat">
+            <div class="d-flex align-items-center">
+                <i class="fas fa-envelope me-3"></i>
+                <p class="m-0 p-0">zadderoyalcrown@gmail.com</p>
+            </div>
+        </div>
+        <div class="col col-lg-2 alamat">
+            <div class="d-flex align-items-center">
+                <i class="fas fa-phone-alt me-3"></i>
+                <p class="m-0 p-0">0811336951</p>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center p-2">
+        <p>&#169; 2023 Zadde Royal Crown, All Right Reserved </p>
+    </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
