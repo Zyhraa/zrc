975b5973696b2e33b729bef2a8ace12785802212
Zhyraa <roralolxd@gmail.com>
2023-02-07 01:12:11 +0700

    l

--- home.php
+++ home.php
@@ -1,4 +1,45 @@
 <?php include 'assets/header.php' ?>
-<div class="bg-image" style="background-image: url('assets/images/home.png'); background-size: cover;  background-repeat:no-repeat; height: 100vh">
+<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+
+<div class="bg-image test" style="background-image: url('assets/images/home.png'); background-size: cover;  background-repeat:no-repeat;">
+   <div class="intro d-flex flex-column">
+      <img src="assets/images/logogold.png" alt="">
+      <p>PT. Zadde Royal Crown, terletak di jakarta, merupakan perusahaan yang bergerak di bidang jasa dan pelayanan umum dengan solusi yang inovatif dan kreatif kepada klien kami dan selalu mengedepankan mutu serta kepercayaan demi kelangsungan bisnis yang harmonis dan berkelanjutan.</p>
+   </div>
+   <div class="jasa">
+      <h1>Our Services</h1>
+      <div class="list d-flex flex-row">
+         <div class="card" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Card title</h5>
+               <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="card-link">Card link</a>
+               <a href="#" class="card-link">Another link</a>
+            </div>
+         </div>
+         <div class="card" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Card title</h5>
+               <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="card-link">Card link</a>
+               <a href="#" class="card-link">Another link</a>
+            </div>
+         </div>
+         <div class="card" style="width: 18rem;">
+            <div class="card-body">
+               <h5 class="card-title">Card title</h5>
+               <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <a href="#" class="card-link">Card link</a>
+               <a href="#" class="card-link">Another link</a>
+            </div>
+         </div>
+      </div>
+      <div class="whyus">
+         <h1>Kenapa pilih kami?</h1>
+         <p>Karena kami adalah pilihan utama dalam memberikan solusi inovatif kreatif dan terpercaya, memberikan kontribusi yang melebihi dari harapan klien atau mitra melalui pelayanan istimewa secara profesional dan intergritas penuh.</p>
+      </div>
+   </div>
 </div>
-<?php include 'assets/footer.php' ?>
