4ae850d0fefb081db6bd9adc463b5c8d2c5b1678
Zhyraa <roralolxd@gmail.com>
2023-02-11 21:39:09 +0700

    hmm

--- visimisi.php
+++ visimisi.php
@@ -4,14 +4,13 @@
 
 <body>
    <?php include 'assets/nav.php'; ?>
-   <div class="bg-image bg">
-      <div class="d-flex flex-row vmm">
-         <div class="col-8 d-flex flex-column vm">
-            <h1>VISI</h1>
-            <p>Menjadi perusahaan yang berkembang dan bermanfaat untuk memenuhi kebutuhan klien atau mitra.</p>
-            <h1>MISI</h1>
-            <p>Produktif dalam membangun bisnis dan aset yang dimiliki secara terintegrasi guna memberikan manfaat dan pelayanan yang luas kepada klien atau mitra</p>
-         </div>
+   <div class="bg-image d-flex flex-row vmm" style="background-image: url('assets/images/visibg.png'); background-size: cover;  background-repeat:no-repeat;">
+      <div class="col-8 d-flex flex-column vm">
+         <h1>VISI</h1>
+         <p>Menjadi perusahaan yang berkembang dan bermanfaat untuk memenuhi kebutuhan klien atau mitra.</p>
+         <h1>MISI</h1>
+         <p>Produktif dalam membangun bisnis dan aset yang dimiliki secara terintegrasi guna memberikan manfaat dan pelayanan yang luas kepada klien atau mitra</p>
+
       </div>
    </div>
 
