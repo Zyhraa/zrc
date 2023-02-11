ae26bfa2bf9311d33d61746837f7e8846320db5f
Aurora <87532858+Zyhraa@users.noreply.github.com>
2023-02-11 21:03:01 +0700

    okok

--- assets/nav.php
+++ assets/nav.php
@@ -2,7 +2,6 @@
 
 <nav class="navbar navbar-expand-lg fs-3" style="background-color: #5C5024;">
     <div class=" container-fluid">
-        <a class="navbar-brand" href="#"><img class="imgnav" src="assets/images/smallogo.png" style="width: 50px;" alt="logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -17,6 +16,7 @@
                 <li class="nav-item mx-5">
                     <a class="nav-link" href="#" style="color: white;">TEAM</a>
                 </li>
+            </ul>
             </ul>
         </div>
     </div>
--- home.php
+++ home.php
@@ -4,22 +4,27 @@
 
 <body>
    <?php include 'assets/nav.php'; ?>
-   <div class="bg-image test" style="background-image: url('assets/images/home1.png'), url('assets/images/home2.png'); background-size: cover;  background-repeat:no-repeat;">
-      <div class="intro d-flex flex-column">
-         <img src="assets/images/logogold.png" alt="">
-         <p>PT. Zadde Royal Crown, terletak di jakarta, merupakan perusahaan yang bergerak di bidang jasa dan pelayanan umum dengan solusi yang inovatif dan kreatif kepada klien kami dan selalu mengedepankan mutu serta kepercayaan demi kelangsungan bisnis yang harmonis dan berkelanjutan.</p>
+   <div class="bg-image test" style="background-image: url('assets/images/home1.png'), url('assets/images/home1.png'); background-size: cover;  background-repeat:no-repeat;">
+      <div class="text-center">
+         <div class="d-flex justify-content-center">
+            <img src="assets/images/logogold.png" width="400px">
+         </div>
+         <p class="text-light">PT. Zadde Royal Crown, terletak di jakarta, merupakan perusahaan yang bergerak di bidang jasa dan pelayanan umum dengan solusi yang inovatif dan kreatif kepada klien kami dan selalu mengedepankan mutu serta kepercayaan demi kelangsungan bisnis yang harmonis dan berkelanjutan.</p>
       </div>
       <div class="jasa">
          <h1>Our Services</h1>
-         <div class="offset-3 col-6">
-            <div class="d-flex flex-wrap">
-               <div class="hex top1">ok</div>
-               <div class="hex">ok</div>
-               <div class="hex top2">ok</div>
-               <div class="hex top2">ok</div>
-               <div class="hex top2">ok</div>
-               <div class="hex top1 top2">ok</div>
-               <div class="hex top2">ok</div>
+         <div class="d-flex">
+            <div class="jarak">&nbsp;</div>
+            <div class="offset-3 col-6 move">
+               <div class="d-flex flex-wrap">
+                  <div class="hex top1">ok</div>
+                  <div class="hex">ok</div>
+                  <div class="hex top2">ok</div>
+                  <div class="hex top2">ok</div>
+                  <div class="hex top2">ok</div>
+                  <div class="hex top1 top2">ok</div>
+                  <div class="hex top2">ok</div>
+               </div>
             </div>
          </div>
          <div class="whyus">
