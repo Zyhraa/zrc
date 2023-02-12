e2ccaf68852bf5b66799e8d46c97f54a41690b1f
Aurora <87532858+Zyhraa@users.noreply.github.com>
2023-02-12 19:09:10 +0700

    splash

--- index.php
+++ index.php
@@ -1,50 +1,38 @@
 <?php
 include 'assets/header.php';
 ?>
-<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+<link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 
-<body class="img-fluid test" style="background-image: url('assets/images/splashh.png') ; background-size: cover; background-repeat: no-repeat;">
-    <div class=" container-fluid">
-        <div class="row">
-            <div class="col-12 my-3">
-                <img src="assets/images/graylogo.png" class="logo img-fluid">
-            </div>
+<body style="background-image: url('assets/images/splashh.png') ; background-size: cover; background-repeat: no-repeat;">
+    <div class="all">
+        <div>
+            <img src="assets/images/graylogo.png" class="logo">
         </div>
-        <div class="container">
-            <div class="column center p-5 m-5">
-                <div class="mb-5">
-                    <h1>ZADDE ROYAL CROWN</h1>
-                    <p>Your Journey Starts Here</p>
-                </div>
-                <a class="mt-5 btna text-decoration-none" style="color:white;" href="home.php">
-                    <div class="offset-5 col-2 px-5 py-3 btnku">About Us</div>
-                </a>
-
-            </div>
+        <div class="center1">
+            <h1>ZADDE ROYAL CROWN</h1>
+            <p>Your Journey Starts Here</p>
+            <a class="btna text-decoration-none" style="color:white;" href="home.php">
+                <div class="py-3 btnku center1">About Us</div>
+            </a>
         </div>
-    </div>
 
-    <div class="container fixed-bottom mb-3">
-        <div class="row justify-content-md-center">
-            <div class="col col-lg-6 alamat">
-                <i class="fas fa-map-marker-alt me-3"></i>
+        <div class="center2">
+            <div class="col col-6 alamat">
+                <i class="fas fa-map-marker-alt m-3"></i>
                 <p>MTH Square Ground Floor (GF) A4 A, Jl. Letjen
                     M. T. Haryono Kav. 10,Jakarta Timur, 13330</p>
             </div>
             <div class="col col-4 alamat">
-                <i class="fas fa-envelope me-3"></i>
+                <i class="fas fa-envelope m-3"></i>
                 <p>zadderoyalcrown@gmail.com</p>
             </div>
-            <div class="col col-lg-2 alamat">
-                <i class="fas fa-phone-alt me-3"></i>
+            <div class="col col-2 alamat">
+                <i class="fas fa-phone-alt m-3"></i>
                 <p>0811336951</p>
             </div>
         </div>
     </div>
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
