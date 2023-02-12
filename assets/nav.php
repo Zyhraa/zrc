c4e69dd880340691a2845edfee33e3415c95a184
Aurora <87532858+Zyhraa@users.noreply.github.com>
2023-02-12 19:54:25 +0700

    update version

--- assets/nav.php
+++ assets/nav.php
@@ -1,24 +1,28 @@
-<?php include 'header.php' ?>
+<?php include 'assets/header.php'; ?>
 <link rel="stylesheet" type="text/css" href="css/nav.css">
+</head>
 
-<nav class="navbar navbar-expand-lg fs-3" style="background-color: #5C5024;">
-    <div class=" container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item mx-5">
-                    <a class="nav-link" href="#" style="color: white;">HOME</a>
-                </li>
-                <li class="nav-item mx-5">
-                    <a class="nav-link" href="visimisi.php" style="color: white;">VISI & MISI</a>
-                </li>
-                <li class="nav-item mx-5">
-                    <a class="nav-link" href="#" style="color: white;">TEAM</a>
-                </li>
-            </ul>
-            </ul>
+<body>
+    <!-- ini nav -->
+    <nav class="navbar navbar-expand-lg fs-3" style="background-color: #5C5024;">
+        <div class=" container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-5">
+                        <a class="nav-link" href="#" style="color: white;">HOME</a>
+                    </li>
+                    <li class="nav-item mx-5">
+                        <a class="nav-link" href="visimisi.php" style="color: white;">VISI & MISI</a>
+                    </li>
+                    <li class="nav-item mx-5">
+                        <a class="nav-link" href="#" style="color: white;">TEAM</a>
+                    </li>
+                </ul>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+    <!-- ini nav -->
--- visimisi.php
+++ visimisi.php
@@ -1,9 +1,34 @@
 <?php include 'assets/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="css/nav.css">
 <link rel="stylesheet" type="text/css" href="css/visimisi.css">
 </head>
 
 <body>
-   <?php include 'assets/nav.php'; ?>
+   <!-- ini nav -->
+   <nav class="navbar navbar-expand-lg fs-3" style="background-color: #5C5024;">
+      <div class=" container-fluid">
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+               <li class="nav-item mx-5">
+                  <a class="nav-link" href="#" style="color: white;">HOME</a>
+               </li>
+               <li class="nav-item mx-5">
+                  <a class="nav-link" href="visimisi.php" style="color: white;">VISI & MISI</a>
+               </li>
+               <li class="nav-item mx-5">
+                  <a class="nav-link" href="#" style="color: white;">TEAM</a>
+               </li>
+            </ul>
+            </ul>
+         </div>
+      </div>
+   </nav>
+   <!-- ini nav -->
+
+
    <div class="bg-image d-flex flex-row vmm" style="background-image: url('assets/images/visibg.png'); background-size: cover;  background-repeat:no-repeat;">
       <div class="col-8 d-flex flex-column vm">
          <h1>VISI</h1>
