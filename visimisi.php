1f57aa78c7e807d4dcca02f00915ace10fe70a00
Zhyraa <roralolxd@gmail.com>
2023-02-12 20:32:18 +0700

    h

--- visimisi.php
+++ visimisi.php
@@ -5,7 +5,7 @@
 
 <body>
    <!-- ini nav -->
-   <nav class="navbar navbar-expand-lg fs-3">
+   <nav class="navbar navbar-expand-lg fs-4">
       <div class=" container-fluid">
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -13,7 +13,7 @@
          <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
             <ul class="navbar-nav">
                <li class="nav-item mx-5">
-                  <a class="nav-link" href="#" style="color: white;">HOME</a>
+                  <a class="nav-link" href="home.php" style="color: white;">HOME</a>
                </li>
                <li class="nav-item mx-5">
                   <a class="nav-link" href="visimisi.php" style="color: white;">VISI & MISI</a>
