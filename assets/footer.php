a12ce39e3849d001f4a8192f82b1a7abbfcf3df3
Aurora <87532858+Zyhraa@users.noreply.github.com>
2023-02-13 17:23:54 +0700

    wrong del

---
+++ assets/footer.php
@@ -0,0 +1,53 @@
+<footer class="mt-auto">
+    <div style="background-color: #5C5024; color:white">
+        <div class="fcenter2">
+            <div class="falamat col-4">
+                <i class="fas fa-map-marker-alt"></i>
+                <p>MTH Square Ground Floor (GF) A4 A, Jl. Letjen
+                    M. T. Haryono Kav. 10,Jakarta Timur, 13330</p>
+            </div>
+            <div class="falamat">
+                <i class="fas fa-envelope"></i>
+                <p>zadderoyalcrown@gmail.com</p>
+            </div>
+            <div class="falamat">
+                <i class="fas fa-phone-alt"></i>
+                <p>0811336951</p>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <p>&#169; 2023 Zadde Royal Crown, All Right Reserved </p>
+        </div>
+    </div>
+    <!-- <div class="container d-flex align-items-center pt-2">
+            <div class="col ms-3 col-5 alamat">
+                <div class="d-flex col-8 align-items-center">
+                    <i class="fas fa-map-marker-alt me-3"></i>
+                    <p class="m-0 p-0 " style="font-family: ' News Cycle', sans-serif;">MTH Square Ground Floor (GF) A4 A, Jl. Letjen
+                        M. T. Haryono Kav. 10,Jakarta Timur, 13330</p>
+                </div>
+            </div>
+            <div class="col col-4 alamat">
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-envelope me-3"></i>
+                    <p class="m-0 p-0" style="font-family: ' News Cycle', sans-serif;">zadderoyalcrown@gmail.com</p>
+                </div>
+            </div>
+            <div class="col col-lg-2 alamat">
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-phone-alt me-3"></i>
+                    <p class="m-0 p-0" style="font-family: ' News Cycle', sans-serif;">0811336951</p>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center p-2">
+            <p style="font-family: ' News Cycle', sans-serif;">&#169; 2023 Zadde Royal Crown, All Right Reserved </p>
+        </div> -->
+    </div>
+
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
