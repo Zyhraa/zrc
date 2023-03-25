eb063288a1d03fe32200a291607d04c36dd1bcd1
Zhyraa <roralolxd@gmail.com>
2023-03-25 21:47:42 +0700

    a

--- assets/footer.php
+++ assets/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div style="background-color: #5C5024; color:white">
         <div class="fcenter2 d-flex justify-content-center pt-3">
-            <div class="falamat     ">
+            <div class="falamat">
                 <i class="fas fa-map-marker-alt"></i>
                 <p>MTH Square Ground Floor (GF) A4 A, Jl. Letjen
                     M. T. Haryono Kav. 10,Jakarta Timur, 13330</p>
@@ -15,9 +15,10 @@
                 <p>0811336951</p>
             </div>
         </div>
-        <div class="d-flex justify-content-center mt-2 cr">
+        <div class="d-flex justify-content-center mt-2 ms-3 cr">
             <p>&#169; 2023 Zadde Royal Crown, All Right Reserved </p>
         </div>
+
     </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
