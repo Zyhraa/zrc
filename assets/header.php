1dabaa9d7db5ebc73726f03d1e668474a1e4be85
Zhyraa <roralolxd@gmail.com>
2023-03-25 21:39:32 +0700

    font weight, font

--- assets/header.php
+++ assets/header.php
@@ -10,7 +10,7 @@
     <!-- font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@700&family=IM+Fell+Great+Primer+SC&family=News+Cycle:wght@400;700&family=Playfair+Display:wght@600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&Rubik:wght@700&family=IM+Fell+Great+Primer+SC&family=News+Cycle:wght@400;700&family=Playfair+Display:wght@600;700;800;900&display=swap" rel="stylesheet">
     <!-- font -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
